4981370076469a1570dbb856ef1f06d44214dc6a
sabiss <sabrinabezsilva@gmail.com>
2025-11-12 12:04:54 -0300

    FEAT: aumentar a capacidade da Sequencia

--- src/sequencia/sequencia.h
+++ src/sequencia/sequencia.h
@@ -16,4 +16,17 @@ class Sequencia{
         ~Sequencia(){//é preciso dizer ao pc "essa memória antes alocada com valores que quero guardar está livre pra você usar e substituir por algo novo"
             delete[] dados;
         }
+        
+    private:
+        void aumentarCapacidade(){
+            capacidade*=2;
+            T *novaSequencia = new T[capacidade];//crio um novo espaço na memória com a capacidade nova
+
+            for(int i = 0; i < quantDados; i++){//passo todos os dados já cadastrado pra ela
+                novaS
+                equencia[i] = dados[i];
+            }
+            delete[] dados;//limpo aquela memoria reservada;
+            dados = novaSequencia;//agora o array de dados apontar pra essa nova Sequencia de capacidade maior;
+        }
 };
