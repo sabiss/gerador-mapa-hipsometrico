da7020fe23681cdcdd85cd17018db07fa1238402
sabiss <sabrinabezsilva@gmail.com>
2025-11-12 16:50:34 -0300

    REFACTOR: métodos de buscar e editar com os operators

--- src/sequencia/sequencia.h
+++ src/sequencia/sequencia.h
@@ -30,30 +30,29 @@ class Sequencia{
         void removerUltimoDado(){
             quantDados--;
         }
-        T buscarElementoViaIndex(int index){
+
+        T operator[](int index) const {//BUSCAR  | o const faz o retorno ser constante, logo, retorna uma cópia do tipo T, ou seja, não será possível editar o array assim
             if(index>=quantDados || index<0){//não há elemento armazenado nesse index ou index negativo
                 std::cerr<<"Erro! Index inválido para capacidade do array";
             }
             return dados[index];
         }
 
-        T operator[](int index) const {
+        T& operator[](int index){//EDITAR  | &T por que retorna uma referência(&) a um tipo T
             if(index>=quantDados || index<0){//não há elemento armazenado nesse index ou index negativo
                 std::cerr<<"Erro! Index inválido para capacidade do array";
             }
-            return dados[index];
+            return dados[index];//por causa do & retorna a referência, então é editável
         }
-
-        T& operator[](int index){
-            if(index>=quantDados || index<0){//não há elemento armazenado nesse index ou index negativo
-                std::cerr<<"Erro! Index inválido para capacidade do array";
-            }
-            return dados[index];
-        }
-
-        void editarElementoViaIndex(int index, T novoElemento){
-            dados[index] = novoElemento;
-        }
+        // T buscarElementoViaIndex(int index){
+        //     if(index>=quantDados || index<0){//não há elemento armazenado nesse index ou index negativo
+        //         std::cerr<<"Erro! Index inválido para capacidade do array";
+        //     }
+        //     return dados[index];
+        // }
+        // void editarElementoViaIndex(int index, T novoElemento){
+        //     dados[index] = novoElemento;
+        // }
     private:
         void aumentarCapacidade(){
             capacidade*=2;
