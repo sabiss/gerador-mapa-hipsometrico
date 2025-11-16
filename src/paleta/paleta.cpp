11ec3e61f4cee59e4dc1ebb1665e4dbaa64dc072
Mylena Avellar <mylenatavellar@gmail.com>
2025-11-16 13:31:13 -0300

    fix: organização do que é definição e implementação nos arquivos .h e .cpp

---
+++ src/paleta/paleta.cpp
@@ -0,0 +1,54 @@
+#include "paleta.h";
+
+
+int Paleta::hexChar(char caractere) {
+    if (caractere >= '0' && caractere <= '9') {
+        return caractere - '0';
+    } 
+    
+    // É necessário validar se o caractere é uma letra minúscula?
+    switch (caractere) {
+    case 'a': return a;
+    case 'b': return b;
+    case 'c': return c;
+    case 'd': return d;
+    case 'e': return e;
+    case 'f': return f;
+    }
+}
+
+int Paleta::lerDuplaHex(const std::string& s, int i) {
+    return hexChar(s[i]) * 16 + hexChar(s[i + 1]);
+}
+
+Cor Paleta::lerCorHex(const std::string& linha) {
+    std::string hex = linha;
+    if (hex[0] == '#'){
+        hex = hex.substr(1);
+    }
+
+    Cor cor;
+    cor.R = lerDuplaHex(hex, 0);
+    cor.G = lerDuplaHex(hex, 2);
+    cor.B = lerDuplaHex(hex, 4);
+
+    return cor;
+}
+
+Paleta::Paleta() {
+        quantidade = 0;
+}
+
+Paleta::Paleta(const std::string filename) {
+    quantidade = 0;
+    std::ifstream file(filename);
+
+    if (!file.is_open()) return;
+
+    std::string corHexa;
+    while (file >> corHexa) {
+        Cor cor = lerCorHex(corHexa);
+        cores.adicionar(cor);
+        quantidade++;
+    }
+}
