303c81f1271cbb0f3d9b9b5d314f16e5451ce611
sabiss <sabrinabezsilva@gmail.com>
2025-11-19 08:13:42 -0300

    FEAT: construtor e destrutor

--- src/terreno/terreno.cpp
+++ src/terreno/terreno.cpp
@@ -1,1 +1,25 @@
 #include "terreno.h"
+#include <cmath>//biblioteca do pow
+
+Terreno::Terreno(int dimensao=3) {
+    dimensaoMatriz = calcularDimensao(dimensao);
+
+    altitudes = new float*[dimensaoMatriz];
+    for(int linha = 0; linha < dimensaoMatriz; linha++){
+        altitudes[linha] = new float[dimensaoMatriz];
+        for(int coluna = 0; coluna < dimensaoMatriz; coluna++){
+            altitudes[linha][coluna] = 0.0f;
+        }
+    }
+};
+
+Terreno::~Terreno(){
+    for(int linha = 0; linha < dimensaoMatriz; linha++){
+        delete[] altitudes[linha];
+    }
+    delete[] altitudes;
+    altitudes = nullptr;
+}
+int Terreno::calcularDimensao(int valorDimensao){
+    return (pow(2, valorDimensao)+1);
+};
