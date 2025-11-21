20f596323236d0210301565813694bb76060fadd
sabiss <sabrinabezsilva@gmail.com>
2025-11-21 20:27:58 -0300

    REFACTOR: espaçamento

--- src/terreno/terreno.cpp
+++ src/terreno/terreno.cpp
@@ -45,9 +45,11 @@ void Terreno::gerarMapa(int n, float rugosidade) {
 float Terreno::obterAltitude(int linha, int coluna){
     return altitudes[linha][coluna];
 }
+
 int Terreno::obterProfundidade(){
     return dimensaoMatriz;
 }
+
 void Terreno::inicializarExtremos(float amplitude){
     altitudes[0][0] = gerarNumeroAleatorio(amplitude);
     altitudes[0][dimensaoMatriz-1] = gerarNumeroAleatorio(amplitude);
