8847a66782ec273fc08dfcbfc0894a0e4462653a
sabiss <sabrinabezsilva@gmail.com>
2025-12-01 08:50:02 -0300

    REFACTOR: algusn tratamentos de erro

--- main.cpp
+++ main.cpp
@@ -14,11 +14,31 @@ int main(){
     cout <<"Informe o tamanho do mapa hipsometrico:\n";
     cin >> tamanhoMapa;
 
+    if (tamanhoMapa < 0) {
+        while(true){
+            cout << "ERRO: Tamanho deve ser entre 1 e 10\n";
+            cout <<"Informe o tamanho do mapa hipsometrico:\n";
+            cin >> tamanhoMapa;
+            if(tamanhoMapa>0){break;}
+        }
+    }
+
     cout<<"Informe o nome desejado para o arquivo da imagem do mapa:\n";
     cin >> nomeArquivoSaidaMapa;
 
+    Paleta paleta(nomeArquivoPaleta);
+    
+     if (paleta.obterTamanho() < 10 || paleta.obterTamanho() > 10) {
+        cout << "ERRO: Paleta precisa ter apenas 10 cores! E tem: " 
+             << paleta.obterTamanho() << endl;
+        while(true){
+
+        }
+    }
     Terreno terreno(tamanhoMapa);
-    terreno.gerarMapa(tamanhoMapa, 0.5);
+    terreno.gerarMapa(0.5);
+    Imagem imagemMapa = terreno.gerarImagem(paleta);
+    imagemMapa.salvarPPM(nomeArquivoSaidaMapa);
 
     return 0;
 }
